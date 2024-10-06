8b4dd83034157a2de79832fce20433fecc6bdda9
g1kwon <spring_shot@naver.com>
2024-10-06 21:42:23 +0900

    StringBuilder - Method Chaining

---
+++ src/lang/String/chaning/MethodChainingMain2.java
@@ -0,0 +1,19 @@
+package lang.String.chaning;
+
+public class MethodChainingMain2 {
+
+    public static void main(String[] args) {
+        ValueAdder adder = new ValueAdder();
+        ValueAdder adder1 = adder.add(1);
+        ValueAdder adder2 = adder.add(2);
+        ValueAdder adder3 = adder.add(3);
+
+        //자기 자신의 참조값을 반환하기 때문에 결과는 동일하다.
+        int result = adder3.getValue();
+        System.out.println("result = " + result);
+        System.out.println("result = " + adder);
+        System.out.println("result = " + adder1);
+        System.out.println("result = " + adder2);
+        System.out.println("result = " + adder3);
+    }
+}
