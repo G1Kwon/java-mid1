2bf3dfafb9037ab2e793dae7607f45762c2e7fb0
g1kwon <spring_shot@naver.com>
2024-10-08 00:48:54 +0900

    Wrapper class - Auto Boxing

---
+++ src/lang/wrapper/AutoboxingMain2.java
@@ -0,0 +1,15 @@
+package lang.wrapper;
+
+public class AutoboxingMain2 {
+    public static void main(String[] args) {
+        //primitive -> Wrapper (Auto Boxing)
+        int value = 7;
+        Integer boxedValue = value;
+
+        //Wrapper -> Primitive (Auto Unboxing)
+        int unboxedValue = boxedValue;
+
+        System.out.println("boxedValue = " + boxedValue);
+        System.out.println("unboxedValue = " + unboxedValue);
+    }
+}
