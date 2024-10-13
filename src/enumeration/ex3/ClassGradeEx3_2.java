a8b894ceaeae24ad625f0000a2a723c83983ab34
g1kwon <spring_shot@naver.com>
2024-10-13 22:10:13 +0900

    ENUM type

---
+++ src/enumeration/ex3/ClassGradeEx3_2.java
@@ -0,0 +1,15 @@
+package enumeration.ex3;
+
+public class ClassGradeEx3_2 {
+
+    public static void main(String[] args) {
+        int price = 10000;
+        DiscountService discountService = new DiscountService();
+        //객체를 생성하려고 하는 개발자도 있을 것이다
+        //ENUM 타입은 생성이 불가능하다.
+/*        Grade grade= new Grade();
+        int result = discountService.discount(grade, price);
+        System.out.println("grade 등급의 할인 금액 : " + result);*/
+
+    }
+}
